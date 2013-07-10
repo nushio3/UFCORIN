2d7f31de63e25a91e0f5d8c1179ee4bf51af97fd
Takayuki Muranushi <muranushi@gmail.com>
2013-07-10 13:17:40 +0900

    wavelet working

--- exe-src/main.hs
+++ exe-src/main.hs
@@ -1,9 +1,12 @@
 module Main where
 
 import Bindings.Gsl.WaveletTransforms
+import Control.Applicative
+import Control.Monad
 import Foreign.Marshal.Alloc (mallocBytes)
 import Foreign.Marshal.Utils (new)
-import Foreign.Storable (peek, sizeOf)
+import Foreign.Storable (peek, poke, sizeOf)
+import Foreign.Ptr (plusPtr)
 import Foreign.C.String (peekCString)
 import Foreign.C.Types (CDouble)
 
@@ -14,12 +17,27 @@ main = do
   let n :: Int
       n = 256
   
-  ptrData <- mallocBytes (sizeOf (0::CDouble) * n)
-  
-  pgwDau <- peek p'gsl_wavelet_daubechies
+  pData <- mallocBytes (sizeOf (0::CDouble) * n)
+  pAbsCoeff <- mallocBytes (sizeOf (0::CDouble) * n)
+  pWavDau <- peek p'gsl_wavelet_daubechies
 
-  ret <- c'gsl_wavelet_alloc pgwDau 4
+  pWork <- c'gsl_wavelet_workspace_alloc (fromIntegral n)
 
-  nam <- peekCString =<< c'gsl_wavelet_name ret
+  pWavelet <- c'gsl_wavelet_alloc pWavDau 4
 
+  nam <- peekCString =<< c'gsl_wavelet_name pWavelet
   print nam
+  
+  forM_ [0..(n-1)] $ \i -> 
+    poke (plusPtr pData (i * sizeOf (0::CDouble)))
+         (fromIntegral i :: CDouble) 
+          
+  flag <- c'gsl_wavelet_transform_forward pWavelet pData 
+          1 (fromIntegral n) pWork
+
+  forM_ [0..(n-1)] $ \i -> do
+    x <- peek (plusPtr pData (i * sizeOf (0::CDouble)))
+    print (x :: CDouble)     
+
+  print flag
+
