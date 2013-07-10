c2acc094b1f38cfe89491d3d1383e3994fa361ed
Takayuki Muranushi <muranushi@gmail.com>
2013-07-10 12:50:32 +0900

    get from here

--- exe-src/main.hs
+++ exe-src/main.hs
@@ -1,13 +1,21 @@
 module Main where
 
 import Bindings.Gsl.WaveletTransforms
+import Foreign.Marshal.Alloc (mallocBytes)
 import Foreign.Marshal.Utils (new)
-import Foreign.Storable (peek)
+import Foreign.Storable (peek, sizeOf)
 import Foreign.C.String (peekCString)
+import Foreign.C.Types (CDouble)
 
 main :: IO ()
 main = do
   putStrLn "hi"
+  
+  let n :: Int
+      n = 256
+  
+  ptrData <- mallocBytes (sizeOf (0::CDouble) * n)
+  
   pgwDau <- peek p'gsl_wavelet_daubechies
 
   ret <- c'gsl_wavelet_alloc pgwDau 4
