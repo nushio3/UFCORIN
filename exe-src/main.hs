65f1c2eb3f669f17f40bcf0feae4c59be130454a
Takayuki Muranushi <muranushi@gmail.com>
2013-07-10 18:45:29 +0900

    format unknown (>_<)

--- exe-src/main.hs
+++ exe-src/main.hs
@@ -1,16 +1,22 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import Bindings.Gsl.WaveletTransforms
 import Control.Applicative
 import Control.Monad
 import qualified Data.ByteString as BS
+import Data.Monoid ((<>))
+import Data.Word(Word32)
+import qualified Data.Text as Text
+import qualified Data.Text.IO as Text
 import Foreign.Marshal.Alloc (mallocBytes)
 import Foreign.Marshal.Utils (new)
 import Foreign.Ptr (plusPtr, castPtr, Ptr(..))
-import Foreign.Storable (peek, poke, sizeOf)
+import Foreign.Storable (peek, poke, sizeOf, peekElemOff, pokeElemOff)
 import Foreign.C.String (peekCString)
 import Foreign.C.Types (CDouble, CFloat, CChar)
-import System.Endian (toBE32)
+import System.Endian (toBE32, toLE32)
+import Text.Printf
 import Unsafe.Coerce (unsafeCoerce)
 
 main :: IO ()
@@ -20,44 +26,58 @@ main = do
   BS.useAsCString origData analyze
 
 analyze origDataStr = do
+
+
   let n :: Int
-      n = 4096
-      sizeOfHeader :: Int
-      sizeOfHeader = 8640
-      pOrigData :: Ptr CFloat
-      pOrigData = castPtr $ plusPtr origDataStr sizeOfHeader
-
-  forM_ [0..(n*n-1)] $ \i -> do
-    let pHead :: Ptr CFloat
-        pHead = plusPtr pOrigData (i * sizeOf (0::CFloat))
-    x <- peek pHead
-    when (x < -1 && x >= -10000) $ print x
-
-
-x :: IO ()
-x = do
-  let n :: Int
-      n = 10
-  pData <- mallocBytes (sizeOf (0::CDouble) * n)
-  pAbsCoeff <- mallocBytes (sizeOf (0::CDouble) * n)
+      n = 512
+  pData <- mallocBytes (sizeOf (0::CDouble) * n * n)
+  pAbsCoeff <- mallocBytes (sizeOf (0::CDouble) * n * n)
   pWavDau <- peek p'gsl_wavelet_haar
 
-  pWork <- c'gsl_wavelet_workspace_alloc (fromIntegral n)
+  pWork <- c'gsl_wavelet_workspace_alloc (fromIntegral $ n*n)
 
   pWavelet <- c'gsl_wavelet_alloc pWavDau 2
 
   nam <- peekCString =<< c'gsl_wavelet_name pWavelet
   print nam
 
-  forM_ [0..(n-1)] $ \i ->
-    poke (plusPtr pData (i * sizeOf (0::CDouble)))
-         (fromIntegral i :: CDouble)
+  -- zero initialize the data
+  forM_ [0..n*n-1] $ \i ->
+    pokeElemOff pData i (42e99 :: CDouble)
+
+  let nBig :: Int
+      nBig = 4096
+      sizeOfHeader :: Int
+      sizeOfHeader = 8640
+      pOrigData :: Ptr Word32
+      pOrigData = castPtr $ origDataStr `plusPtr` sizeOfHeader
+
+  forM_ [0..(nBig-1)] $ \iy -> do
+    forM_ [0..(nBig-1)] $ \ix -> do
+      val'' <- peekElemOff pOrigData (iy*nBig + ix)
+      let
+        val' :: CFloat
+        val' = unsafeCoerce . toBE32 . unsafeCoerce $ val''
+
+
+        val :: CDouble
+        val
+          | abs(val') < 1e30 = fromRational $ toRational $ val'
+          | otherwise   = 0
+        ix' = ix `div` (nBig `div`n)
+        iy' = iy `div` (nBig `div`n)
+      pokeElemOff pData (iy'*n+ix') val
+
+  bitmapText <- fmap Text.concat $ forM [0..(n-1)] $ \iy -> do
+    lineText <- fmap Text.concat $ forM [0..(n-1)] $ \ix -> do
+      val <- peekElemOff pData (iy*n + ix)
+      return $ Text.pack $ printf "%i %i %s\n" ix iy (show (val :: CDouble))
+    return $ lineText <> "\n"
+
+  Text.writeFile "test.txt" bitmapText
 
   flag <- c'gsl_wavelet_transform_forward pWavelet pData
-          1 (fromIntegral n) pWork
+          1 (fromIntegral $ n*n) pWork
 
-  forM_ [0..(n-1)] $ \i -> do
-    x <- peek (plusPtr pData (i * sizeOf (0::CDouble)))
-    print (x :: CDouble)
 
   print flag
