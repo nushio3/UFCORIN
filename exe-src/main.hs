83ed2b92e0b29c482a0700e464acd15a08e920db
Takayuki Muranushi <muranushi@gmail.com>
2013-07-10 17:25:05 +0900

    possibly this is the right endian.

--- exe-src/main.hs
+++ exe-src/main.hs
@@ -9,36 +9,35 @@ import Foreign.Marshal.Utils (new)
 import Foreign.Ptr (plusPtr, castPtr, Ptr(..))
 import Foreign.Storable (peek, poke, sizeOf)
 import Foreign.C.String (peekCString)
-import Foreign.C.Types (CDouble, CChar)
+import Foreign.C.Types (CDouble, CFloat, CChar)
+import System.Endian (toBE32)
+import Unsafe.Coerce (unsafeCoerce)
 
 main :: IO ()
 main = do
   putStrLn "hi"
   origData <- BS.readFile "resource/sample.fits"
   BS.useAsCString origData analyze
-  
+
 analyze origDataStr = do
   let n :: Int
       n = 4096
       sizeOfHeader :: Int
       sizeOfHeader = 8640
-      pOrigData :: Ptr CDouble
+      pOrigData :: Ptr CFloat
       pOrigData = castPtr $ plusPtr origDataStr sizeOfHeader
-  
+
   forM_ [0..(n*n-1)] $ \i -> do
-    let pHead :: Ptr CDouble
-        pHead = plusPtr pOrigData (i * sizeOf (0::CDouble))
-        pHeadW :: Ptr CChar
-        pHeadW = castPtr pHead
-        pBytes = map (plusPtr pHeadW) [0..3]
-    byteDatas <- mapM peek pBytes
-    zipWithM_ poke pBytes $ reverse (map (*0) byteDatas :: [CChar])
-    
-    
-    x <-peek pHead
-    when (x > 500) $ print x
-  
-  
+    let pHead :: Ptr CFloat
+        pHead = plusPtr pOrigData (i * sizeOf (0::CFloat))
+    x <- peek pHead
+    when (x < -1 && x >= -10000) $ print x
+
+
+x :: IO ()
+x = do
+  let n :: Int
+      n = 10
   pData <- mallocBytes (sizeOf (0::CDouble) * n)
   pAbsCoeff <- mallocBytes (sizeOf (0::CDouble) * n)
   pWavDau <- peek p'gsl_wavelet_haar
@@ -49,17 +48,16 @@ analyze origDataStr = do
 
   nam <- peekCString =<< c'gsl_wavelet_name pWavelet
   print nam
-  
-  forM_ [0..(n-1)] $ \i -> 
+
+  forM_ [0..(n-1)] $ \i ->
     poke (plusPtr pData (i * sizeOf (0::CDouble)))
-         (fromIntegral i :: CDouble) 
-          
-  flag <- c'gsl_wavelet_transform_forward pWavelet pData 
+         (fromIntegral i :: CDouble)
+
+  flag <- c'gsl_wavelet_transform_forward pWavelet pData
           1 (fromIntegral n) pWork
 
   forM_ [0..(n-1)] $ \i -> do
     x <- peek (plusPtr pData (i * sizeOf (0::CDouble)))
-    print (x :: CDouble)     
+    print (x :: CDouble)
 
   print flag
-
