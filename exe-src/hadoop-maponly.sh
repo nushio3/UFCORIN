a4d4a9178d02f39036b3ea9a77f305ea447d37b1
nushio <nushio@dn-m001.back.gion.bbtower.co.jp>
2013-08-26 20:44:25 +0900

    more test.

--- exe-src/hadoop-maponly.sh
+++ exe-src/hadoop-maponly.sh
@@ -5,7 +5,7 @@ cp dist/build/main/main wavelet-mapper
 hadoop jar /opt/cloudera/parcels/CDH/lib/hadoop-0.20-mapreduce/contrib/streaming/hadoop-streaming-2.0.0-mr1-cdh4.3.0.jar \
     -input resource/hdfs-hmi-2011-01.txt \
     -output hadoop-result \
-    -mapper "bash wavelet-mapper" \
+    -mapper "exec wavelet-mapper" \
     -reducer "cat" \
     -file wavelet-mapper
 
