3ef2b74e30d24b4976b5c30c8c229471f3e8c8a2
nushio <nushio@dn-m001.back.gion.bbtower.co.jp>
2013-08-26 19:52:10 +0900

    further test on hadoop system.

--- exe-src/hadoop-test.sh
+++ exe-src/hadoop-test.sh
@@ -5,9 +5,9 @@ cp dist/build/main/main main-mapper
 hadoop jar /opt/cloudera/parcels/CDH/lib/hadoop-0.20-mapreduce/contrib/streaming/hadoop-streaming-2.0.0-mr1-cdh4.3.0.jar \
     -input resource/hdfs-hmi-2011-01.txt \
     -output hadoop-result \
-    -mapper ./main-mapper \
+    -mapper "cat" \
     -reducer "cat" \
-    -file ./main-mapper
+    -file main-mapper
 
 #    -input /user/nushio/filelist/ \
 #/opt/cloudera/parcels/CDH/lib/hadoop-mapreduce/hadoop-streaming-2.0.0-cdh4.3.0.jar \
