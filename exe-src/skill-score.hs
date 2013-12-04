2601928229bb6f50c1fdb7e993a2c1aba315912a
nushio <nushio@nushiohshio.kuins.net>
2013-12-04 19:33:17 +0900

    calculate hss and tss.

--- exe-src/skill-score.hs
+++ exe-src/skill-score.hs
@@ -1,5 +1,34 @@
 #!/usr/bin/env runhaskell
 
+import System.Environment
+import Text.Printf
+
 main :: IO ()
 main = do
-  print "hi"
+  (fnPredict: fnObserve: _) <- getArgs
+  predStr <- readFile fnPredict
+  obsvStr <- readFile fnObserve
+  let preds = map (head . words) $ lines predStr
+      obsvs = map (head . words) $ lines obsvStr
+      
+      count :: Bool -> Bool -> Double
+      count bx by = 
+        fromIntegral $
+        length $ 
+        filter (\(x,y) -> (x/="0")==bx && (y/="0")==by)$
+        zip preds obsvs
+      
+      nTP = count True  True
+      nFN = count False True            
+      nFP = count True  False
+      nTN = count False False
+      
+      hss = 2*(nTP*nTN - nFN*nFP)/
+            ((nTP+nFN)*(nFN+nTN) + (nTP+nFP)*(nFP+nTN))
+      tss = nTP/(nTP+nFN) - nFP/(nFP+nTN)
+      
+  printf "%f\t%f\n" nTP nFP
+  printf "%f\t%f\n" nFN nTN
+  printf "hss=%f\n" hss
+  printf "tss=%f\n" tss  
+  
