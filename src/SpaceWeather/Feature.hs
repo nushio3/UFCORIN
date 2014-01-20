eaf423fd60264140cb012a9df41cb78b3617ea0c
Takayuki Muranushi <muranushi@google.com>
2014-01-20 17:38:56 +0900

    forget to add a file.

---
+++ src/SpaceWeather/Feature.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE FlexibleContexts, FlexibleInstances, MultiParamTypeClasses, TemplateHaskell, TypeSynonymInstances #-}
+module SpaceWeather.Feature where
+
+import Control.Lens
+import Control.Monad
+import qualified Data.Map as Map
+import Data.Monoid
+import qualified Data.Text as T
+import SpaceWeather.Format
+import SpaceWeather.TimeLine
+import Text.Printf
+
+type Feature = TimeLine Double
+
+type Features = TimeLine [Double]
+ 
+instance Format Feature where
+  encode = 
+    T.unlines .
+    map (\(t,a) -> T.unwords [showT t, showT a] ) .
+    Map.toList
+
+  decode txt0 = do
+    -- (Either String) monad
+    let xs = linesWithComment txt0
+        parseLine :: (Int, T.Text) -> Either String (TimeBin, Double)
+        parseLine (lineNum, txt) = 
+          maybe (Left $ printf "parse error on line %d" lineNum) Right $ do
+            -- maybe monad here
+            let wtxt = T.words txt
+            t <- readAt wtxt 0
+            a <- readAt wtxt 1
+            return (t,a)
+    fmap Map.fromList $ mapM parseLine xs
