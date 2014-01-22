4f02079068c61b721c7206e7ea81b46fe42fce92
Takayuki Muranushi <muranushi@google.com>
2014-01-22 14:56:55 +0900

    write to hadoop fs.

---
+++ src/SpaceWeather/CmdArgs.hs
@@ -0,0 +1,1 @@
+module SpaceWeather.CmdArgs where
--- src/System/IO/Hadoop.hs
+++ src/System/IO/Hadoop.hs
@@ -1,2 +1,18 @@
+{-# LANGUAGE TemplateHaskell #-}
 module System.IO.Hadoop where
 
+import Control.Lens
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+
+data FileSystem = LocalFS | HadoopFS
+
+data FSOption = FSOPtion
+  { _targetFS :: FileSystem
+  }
+
+makeClassy ''FSOption
+
+writeHadoopFS :: FilePath -> T.Text -> IO ()
+writeHadoopFS fp txt = do
+  return ()
