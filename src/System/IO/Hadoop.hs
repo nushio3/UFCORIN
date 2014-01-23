b355c8ec9d4c859b7ee9ce8ad689efae3cc9f3d6
Takayuki Muranushi <muranushi@gmail.com>
2014-01-23 10:11:09 +0900

    some modifications

--- src/System/IO/Hadoop.hs
+++ src/System/IO/Hadoop.hs
@@ -4,6 +4,8 @@ module System.IO.Hadoop where
 import Control.Lens
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
+import System.Process
+import Text.Printf
 
 data FileSystem = LocalFS | HadoopFS
 
@@ -13,6 +15,16 @@ data FSOption = FSOPtion
 
 makeClassy ''FSOption
 
+readHadoopFS :: FilePath -> IO T.Text 
+readHadoopFS fp = do
+  (_,Just hout,_,hproc) <- createProcess (shell $ printf "hadoop fs -cat %s" fp){std_out = CreatePipe}
+  ret <- T.hGetContents hout
+  _ <- waitForProcess hproc
+  return ret
+
 writeHadoopFS :: FilePath -> T.Text -> IO ()
 writeHadoopFS fp txt = do
+  (Just hin,_,_,hproc) <- createProcess (shell $ printf "hadoop fs -put -f - %s" fp){std_in = CreatePipe}
+  T.hPutStr hin txt
+  _ <- waitForProcess hproc
   return ()
--- test/FeaturePackSpec.hs
+++ test/FeaturePackSpec.hs
@@ -3,6 +3,7 @@ module FeaturePackSpec where
 import qualified Data.Text as T
 import qualified Data.Map as Map
 import SpaceWeather.Feature
+import SpaceWeather.FeaturePack
 import SpaceWeather.Format
 import Test.Hspec
 import Test.Hspec.QuickCheck
