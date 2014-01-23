d060e178e6c1c20e2c128494fcf5a5fdf77c5e94
Takayuki Muranushi <muranushi@gmail.com>
2014-01-23 11:39:00 +0900

    test write feature

--- test/FeaturePackSpec.hs
+++ test/FeaturePackSpec.hs
@@ -1,5 +1,6 @@
 module FeaturePackSpec where
 
+import Control.Lens
 import Control.Monad.IO.Class
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
@@ -20,12 +21,16 @@ testFS1 = FeatureSchema
   , _schemaFilename = "/user/nushio/wavelet-features/haarC-2-S-0000-0000.txt"
   }
 
+testFS2 :: FeatureSchema
+testFS2 = testFS1 & schemaFileName ^~
+  "/user/nushio/wavelet-features/bsplC-2-S-0000-0000.txt"
+
 testFP :: FeatureSchemaPack
-testFP = FeatureSchemaPack [testFS1]
+testFP = FeatureSchemaPack [testFS1, testFS2]
 
 spec::Spec
 spec = do
   describe "A" $ do
     it "B" $ do
-      liftIO $ T.putStr $ encode testFP
+      liftIO $ T.writeFile "test.fp" $ encode testFP
       (1+1) `shouldBe` 2
