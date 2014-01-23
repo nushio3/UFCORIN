0711a8a91a8d7a210cc31c22fca8d333fdf40511
Takayuki Muranushi <muranushi@gmail.com>
2014-01-23 10:10:53 +0900

    test for feature pack added

---
+++ test/FeaturePackSpec.hs
@@ -0,0 +1,12 @@
+module FeaturePackSpec where
+
+import qualified Data.Text as T
+import qualified Data.Map as Map
+import SpaceWeather.Feature
+import SpaceWeather.Format
+import Test.Hspec
+import Test.Hspec.QuickCheck
+import Test.QuickCheck.Arbitrary
+
+spec::Spec
+spec = return ()
