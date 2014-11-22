ba5377d3102d20a1b4424aaa6616b859e14a5cf6
Ubuntu <ubuntu@ip-172-31-42-26.us-west-2.compute.internal>
2014-11-22 14:18:02 +0000

    add caching mechainsm

--- src/System/IO/Hadoop.hs
+++ src/System/IO/Hadoop.hs
@@ -3,11 +3,14 @@ module System.IO.Hadoop where
 
 import Control.Lens
 import Data.List (isPrefixOf)
+import qualified Data.Map as M
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
 import System.IO
+import System.IO.Unsafe
 import System.Process
 import Text.Printf
+import Data.IORef
 
 data FileSystem = LocalFS | HadoopFS
 
@@ -16,8 +19,27 @@ data FSOption = FSOPtion
   }
 makeClassy ''FSOption
 
+{-# NOINLINE cacheRef #-}
+cacheRef :: IORef (M.Map FilePath T.Text)
+cacheRef = unsafePerformIO $ newIORef M.empty
+
+
+memcached :: (FilePath -> IO T.Text) -> FilePath -> IO T.Text 
+memcached prog input = do
+  cache <- readIORef cacheRef
+  case M.lookup input cache of
+    Nothing -> do
+      resp <- prog input
+      modifyIORef cacheRef $ M.insert input resp
+      return resp
+    Just resp -> return resp
+
+
 readFile :: FilePath -> IO T.Text 
-readFile fp = do
+readFile = memcached readFile'
+
+readFile' :: FilePath -> IO T.Text 
+readFile' fp = do
   (_,Just hout,_,hproc) <- createProcess (shell $ printf "aws s3 cp --region us-west-2 %s -" (compatibleFilePath fp)){std_out = CreatePipe}
   ret <- T.hGetContents hout
   _ <- waitForProcess hproc
