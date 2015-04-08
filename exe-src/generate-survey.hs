fadf583918eaccadfcf9e375db22f56e0d64d148
Ubuntu <ubuntu@ip-172-31-33-91.us-west-2.compute.internal>
2015-04-08 07:40:32 +0000

    add disk cache to UFCORIN.

--- exe-src/generate-survey.hs
+++ exe-src/generate-survey.hs
@@ -1,10 +1,11 @@
-{-# LANGUAGE OverloadedStrings #-}
 module Main where
 import Control.Applicative
 import Control.Lens
 import Control.Monad
 import Data.List
+import qualified Data.Text.IO as T
 import System.Environment
+import System.Process
 import Text.Printf
 
 import SpaceWeather.CmdArgs
@@ -13,14 +14,19 @@ import SpaceWeather.Format
 import SpaceWeather.Prediction
 import SpaceWeather.Regressor.General
 import qualified System.IO.Hadoop as HFS
-import qualified Data.Text.IO as T
+
+surveyDir = "survey3"
 
 main :: IO ()
 main = do
-  sequence_ [process (2^i) (2^j) (2^iy) (2^jy) | i <- [0..9], j <- [i+1..9],  iy <- [0..9], jy <- [iy+1..9] ]
+  system $ "mkdir -p " ++ surveyDir
+  sequence_ [process "bsplC-301" True (2^i) (2^j) (2^iy) (2^jy) | i <- [0..9], j <- [i..9],  iy <- [0..9], jy <- [iy..9] ]
+  sequence_ [process "bsplC-301" False (2^i) (2^j) (2^iy) (2^jy) | i <- [0..9], j <- [i..9],  iy <- [0..9], jy <- [iy..9] ]
+  sequence_ [process "haarC-2" True (2^i) (2^j) (2^iy) (2^jy) | i <- [0..9], j <- [i..9],  iy <- [0..9], jy <- [iy..9] ]
+  sequence_ [process "haarC-2" False (2^i) (2^j) (2^iy) (2^jy) | i <- [0..9], j <- [i..9],  iy <- [0..9], jy <- [iy..9] ]
 
-process :: Int -> Int -> Int -> Int -> IO ()
-process lower upper lowerY upperY = withWorkDir $ do
+process :: String -> Bool -> Int -> Int -> Int -> Int -> IO ()
+process basisName isStd lower upper lowerY0 upperY0 = withWorkDir $ do
   strE <- fmap decode $ T.readFile "resource/strategy-template.yml"
   case strE of
     Left msg -> putStrLn msg
@@ -31,22 +37,40 @@ process lower upper lowerY upperY = withWorkDir $ do
           & predictionSessionFile .~ finalSesFn
           & predictionResultFile .~ finalResFn
           & predictionRegressionFile .~ finalRegFn
-          & featureSchemaPackUsed . fspFilenamePairs %~ (++  (genFn <$> coordList <*> coordListY))
+          & featureSchemaPackUsed . fspFilenamePairs %~ (++ fnPairs)
+
+        lowerY = if isStd then lowerY0 else lower
+        upperY = if isStd then upperY0 else upper
 
         coordList :: [Int]
         coordList = [2^i | i <- [0..9], lower <= 2^i , 2^i <= upper  ]
         coordListY :: [Int]
         coordListY = [2^i | i <- [0..9], lowerY <= 2^i , 2^i <= upperY  ]
 
+        fnPairs
+          | isStd     = genFn <$> coordList <*> coordListY
+          | otherwise = coordList >>= genFnN
+
         genFn :: Int -> Int -> (String,FilePath)
-        genFn x y = ("f35Log", printf "/user/nushio/wavelet-features/bsplC-301-S-%04d-%04d.txt" x y)
+        genFn x y = ("f35Log", printf "/user/nushio/wavelet-features/%s-%04d-%04d.txt" basisString x y)
+
+        genFnN :: Int -> [(String,FilePath)]
+        genFnN x  =
+          [ ("f35Log", printf "/user/nushio/wavelet-features/%s-%04d-%04d.txt" basisString (0::Int) x)
+          , ("f35Log", printf "/user/nushio/wavelet-features/%s-%04d-%04d.txt" basisString x (0::Int))
+          , ("f35Log", printf "/user/nushio/wavelet-features/%s-%04d-%04d.txt" basisString x x)]
+
+        basisString :: String
+        basisString = printf "%s-%s" basisName (if isStd then "S" else "N")
 
         candSesFn = strategy ^. predictionSessionFile
         candResFn = strategy ^. predictionResultFile
         candRegFn = strategy ^. predictionRegressionFile
 
         fn :: String
-        fn = printf "survey2/%04d-%04d-%04d-%04d.yml" (lower :: Int) (upper :: Int) lowerY upperY
+        fn = printf "%s/%s-%04d-%04d-%04d-%04d.yml"
+          surveyDir basisString
+          (lower :: Int) (upper :: Int) lowerY upperY
 
         finalSesFn
           | candSesFn /= "" = candSesFn
--- exe-src/scan-survey-results.hs
+++ exe-src/scan-survey-results.hs
@@ -4,12 +4,15 @@ import Control.Applicative
 import Control.Lens
 import Control.Monad
 import Data.List
+import qualified Data.Map as M
 import System.Environment
 import Text.Printf
 
 import SpaceWeather.CmdArgs
 import SpaceWeather.FeaturePack
 import SpaceWeather.Format
+import SpaceWeather.FlareClass
+import SpaceWeather.SkillScore
 import SpaceWeather.Prediction
 import SpaceWeather.Regressor.General
 import qualified System.IO.Hadoop as HFS
@@ -26,4 +29,8 @@ process fn = do
   Right result0 <- fmap decode $ T.readFile fn
   let result :: PredictionResult
       result = result0
-  print result
+      prm = result ^. predictionResultMap
+      xcm = prm M.! XClassFlare M.! TrueSkillStatistic
+      xcsv = xcm ^. scoreValue
+
+  print xcsv
