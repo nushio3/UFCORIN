03e93132c0e20c8ec10a324f764775fb6e3c3573
Ubuntu <ubuntu@ip-172-31-37-237.us-west-2.compute.internal>
2015-07-09 11:13:19 +0000

    modif

--- script/suntomorrow/train_autoencoder.py
+++ script/suntomorrow/train_autoencoder.py
@@ -37,8 +37,9 @@ for fn in stdout.split('\n'):
 
 conv1 = F.Convolution2D(1,1,3,stride=1,pad=1)
 conv2 = F.Convolution2D(1,1,3,stride=1,pad=1)
+conv3 = F.Convolution2D(1,1,3,stride=1,pad=1)
 
-model=chainer.FunctionSet(l1 = conv1, l2 = conv2)
+model=chainer.FunctionSet(l1 = conv1, l2 = conv2, l3 = conv3)
 
 if gpu_flag:
     cuda.init(0)
@@ -49,14 +50,15 @@ if gpu_flag:
 def forward(x_data,train=True):
     x = Variable(x_data, volatile = not train)
     h1 = F.dropout(F.relu(model.l1(x)),  train=train)
-    h2 = F.dropout(F.relu(model.l2(h1)), train=train)
-    return F.mean_squared_error(x,h2)
+    h2 = F.dropout(F.tanh(model.l2(h1)), train=train)
+    h3 = model.l3(h2)
+    return F.mean_squared_error(x,h3)
 
 
 optimizer = optimizers.Adam()
 optimizer.setup(model.collect_parameters()) 
 
-for epoch in range(10000):
+for epoch in range(1000000):
     batch = []
     for i in range(50):
         batch.append(random.choice(sun_data))
