f6f0ec4c775a9337a1c5ce169b5add6c70c29ff0
Ubuntu <ubuntu@ip-172-31-16-68.us-west-2.compute.internal>
2015-08-05 02:07:38 +0000

    goes downloader

---
+++ script/goes/goes-monitor.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import datetime
+import re
+import sqlalchemy as sql
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+with open('.mysqlpass','r') as fp:
+    password = fp.read().strip()
+
+Base = declarative_base()
+
+class GOES(Base):
+    __tablename__ = 'goes_xray_flux'
+
+    t = sql.Column(sql.DateTime, primary_key=True)
+    xray_flux_long = sql.Column(sql.Float)
+    xray_flux_short = sql.Column(sql.Float)
+
+
+
+
+
+engine = sql.create_engine('mysql+mysqldb://ufcoroot:{}@sun-feature-db.cvxxbx1dlxvk.us-west-2.rds.amazonaws.com:3306/sun_feature'.format(password))
+if False:
+    GOES.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+with open('goes-xray-flux-primary.txt', 'r') as fp:
+    con = fp.read()
+    lines = con.split('\n')
+    for l in lines:
+        match = re.search('^(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\S+)\s+(\S+)',l)
+        if not match : continue
+        year=int(match.group(1))
+        month=int(match.group(2))
+        day=int(match.group(3))
+        hour=int(match.group(4)[0:2])
+        minute=int(match.group(4)[2:4])
+        flux_short=float(match.group(7))
+        flux_long=float(match.group(8))
+        
+        goes = GOES(t=datetime.datetime(year,month,day,hour,minute,0), xray_flux_long=flux_long, xray_flux_short=flux_short)
+        session.merge(goes)
+session.commit()
