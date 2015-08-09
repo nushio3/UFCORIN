c06135b700094f1c35276d935e05cc630305be36
Ubuntu <ubuntu@ip-172-31-16-68.us-west-2.compute.internal>
2015-08-09 07:23:18 +0000

    hmi downloader & push to database.

--- script/jsoc/download-hmi-nrt.py
+++ script/jsoc/download-hmi-nrt.py
@@ -13,6 +13,9 @@ from sqlalchemy.ext.declarative import declarative_base
 import wavelet
 
 
+with open(os.path.expanduser('~')+'/.mysqlpass','r') as fp:
+    password = fp.read().strip()
+
 
 def system(cmd):
     subprocess.call(cmd, shell=True)
@@ -29,10 +32,21 @@ os.chdir(workdir)
 
 # Download the latest NRT FITS image
 
-query = "hmi.M_720s_nrt[$]"
+series_name = "hmi.M_720s_nrt"
+query = series_name + "[$]"
 command = path+"exportfile.csh '"+query+ "' " + sys.argv[1]
 print command
-#system(command)
+system("rm *.fits")
+system(command)
+
+# create the DB class for our particular wavelet
+DB = wavelet.db_class(series_name, 'haar')
+engine = sql.create_engine('mysql+mysqldb://ufcoroot:{}@sun-feature-db.cvxxbx1dlxvk.us-west-2.rds.amazonaws.com:3306/sun_feature'.format(password))
+DB.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
 
 def fits2npz(newfn, npzfn):
     hdulist=fits.open(newfn)
@@ -47,9 +61,10 @@ def fits2npz(newfn, npzfn):
             r2 = (x-x0)**2 + (y-y0)**2
             r0 = 1800.0*zoom_ratio
             if r2 >= r0**2 : img2[y][x]=0.0
+    img32 = np.float32(img2)
+    np.savez_compressed(npzfn, img=img32)
 
-    np.savez_compressed(npzfn, img=np.float32(img2))
-    register_wavelet(img2, newfn.replace('.fits','.png'))
+    return img32
         
 
 def plot_img(img,fn,title_str):
@@ -68,9 +83,9 @@ def plot_img(img,fn,title_str):
 
 def register_wavelet(img, imgfn):
     plot_img(img,imgfn,"real")
-    wavelet_img = wavelet.wavedec2_img_NS(img,'haar')
+    wavelet_img = wavelet.wavedec2_img(img,'haar','NS')
     plot_img(wavelet_img,"NS_" + imgfn,"NS-wavelet")
-    wavelet_img = wavelet.wavedec2_img_S(img,'haar')
+    wavelet_img = wavelet.wavedec2_img(img,'haar','S')
     plot_img(wavelet_img,"S_" + imgfn,"S-wavelet")
     
 
@@ -79,17 +94,18 @@ for fn in glob.glob('*.fits'):
 
     ma = re.search('%5B(\d+)\.(\d+)\.(\d+)_(\d+)%3A(\d+)',fn)
     if not ma: continue
-    yyyy=ma.group(1)
-    mm=ma.group(2)
-    dd=ma.group(3)
-    hh=ma.group(4)
-    minu=ma.group(5)
+    yyyy=int(ma.group(1))
+    mm  =int(ma.group(2))
+    dd  =int(ma.group(3))
+    hh  =int(ma.group(4))
+    minu=int(ma.group(5))
     
-    print 'detect {}-{}-{} {}:{}'.format(yyyy,mm,dd,hh,minu)
+    t=datetime.datetime(yyyy,mm,dd,hh,minu)
+    print 'detect {:04}-{:02}-{:02} {:02}:{:02}'.format(yyyy,mm,dd,hh,minu)
 
-    newfn=hh+minu+'.fits'
+    newfn='{:02}{:02}.fits'.format(hh,minu)
     shutil.copy(fn,newfn)
-    s3 = "aws s3 cp "+newfn+" s3://sdo/hmi/mag720/"+yyyy+"/"+mm+"/"+dd+"/"+newfn
+    s3 = "aws s3 cp "+newfn+" s3://sdo/hmi/mag720/{:04}/{:02}/{:02}/{}".format(yyyy,mm,dd,newfn)
     print s3
     system(s3)
 
@@ -98,11 +114,15 @@ for fn in glob.glob('*.fits'):
     zoom_ratio = float(reso_new)/reso_original
 
     npzfn = newfn.replace('.fits','.npz')
-    fits2npz(newfn, npzfn)
+    img = fits2npz(newfn, npzfn)
 
-    cmd='aws s3 cp '+npzfn+ (' s3://sdo/hmi/mag720x{}/{}/{}/{}/'.format(reso_new,yyyy,mm,dd))+npzfn
+    cmd='aws s3 cp '+npzfn+ (' s3://sdo/hmi/mag720x{}/{:04}/{:02}/{:02}/'.format(reso_new,yyyy,mm,dd))+npzfn
     print cmd
     system(cmd)
 
+    r = DB()
+    r.fill_columns(t, img)
 
-
+    print r
+    session.merge(r)
+    session.commit()
--- script/jsoc/wavelet.py
+++ script/jsoc/wavelet.py
@@ -2,6 +2,97 @@
 
 import pywt
 import numpy as np
+import sqlalchemy as sql
+from sqlalchemy.ext.declarative import declarative_base
+
+
+def db_fill_columns(self,t, img):
+    self.t = t
+    for s in ['S','NS']:
+        wv_img = wavedec2_img(img, self.wavelet_name, s)
+        for r in subspaces(s):
+            subimg = wv_img[r.t : r.b , r.l : r.r]
+            key = "mean_" + s + "_" + r.key_string()
+            val = np.sum(subimg)/r.area()
+            setattr(self, key, val)
+            key = "mean_sq_" + s + "_" + r.key_string()
+            val = np.sum(subimg**2)/r.area()
+            setattr(self, key, val)
+            
+def db_class_name(series_name, wavelet): 
+    return "DB_" +(series_name + "_wavelet_" + wavelet).replace('.','_')
+
+def db_class(series_name, wavelet):
+    table_name = db_class_name(series_name, wavelet)
+    Base = declarative_base()
+    class Ret(Base):
+        __tablename__ = table_name
+        t = sql.Column(sql.DateTime, primary_key=True)
+        wavelet_name = wavelet
+
+    for s in ['S','NS']:
+        for r in subspaces(s):
+            key = "mean_" + s + "_" + r.key_string()
+            setattr(Ret, key, sql.Column(sql.Float))
+            key = "mean_sq_" + s + "_" + r.key_string()
+            setattr(Ret, key, sql.Column(sql.Float))
+    Ret.fill_columns = db_fill_columns
+    return Ret
+
+
+
+
+
+class WaveletSubspace:
+    def __init__ (self,l,t,r,b):
+        # left, top, right, bottom
+        self.l=l; self.t=t; self.r=r; self.b=b
+    def key_string(self):
+        return '{:04}_{:04}_{:04}_{:04}'.format(self.l,self.t,self.r,self.b)
+    def area(self):
+        return (self.r-self.l)*(self.b-self.t)
+    def overwrap(self, other):
+        if self.r <= other.l : return False
+        if self.l >= other.r : return False
+        if self.b <= other.t : return False
+        if self.t >= other.b : return False
+        return True
+
+def subspaces(w2d_ordering):
+    ret = []
+    if w2d_ordering=='S':
+        for iy in range(11):
+            for ix in range(11):
+                l=int(2**(ix-1)); t=int(2**(iy-1)); 
+                r=2**ix; b=2**iy; 
+                ret.append(WaveletSubspace(l,t,r,b))
+    elif w2d_ordering=='NS':
+        ret.append(WaveletSubspace(0,0,1,1))
+        for i in range(10):
+            lo=int(2**i)
+            hi=int(2**(i+1))
+            ret.append(WaveletSubspace(lo,0 ,hi,lo))
+            ret.append(WaveletSubspace(0 ,lo,lo,hi))
+            ret.append(WaveletSubspace(lo,lo,hi,hi))
+    else:
+        raise Exception('w2d_ordering should be either "S" or "NS"; got : ' + w2d_ordering)
+    return ret
+
+
+if __name__ == '__main__':
+    # test if subspaces don't overwrap, and covers all area
+    overwrap_found=False
+    for s in ['S','NS']:
+        sum = 0
+        ss = subspaces(s)
+        for r in ss:
+            sum += r.area()
+        assert( sum == 1024**2)
+        
+        for i in range(len(ss)):
+            for j in range(i+1,len(ss)):
+                if ss[i].overwrap(ss[j]) : overwrap_found=True
+    assert(not overwrap_found)
 
 def concat_w2d(ws):
     if (len(ws)==1) : return ws[0]
@@ -16,14 +107,16 @@ def concat_w2d(ws):
     
     return concat_w2d([ca2] + ws[2:])
 
-def wavedec2_img_NS(img, wavelet):
-    ws = pywt.wavedec2(img,'haar')
-    return concat_w2d(ws)
-
-def wavedec2_img_S(img, wavelet):
-    for t in range(2):
-        for y in range(np.shape(img)[1]):
-            img[y] = np.concatenate(pywt.wavedec(img[y], wavelet))
-        img = np.transpose(img)
-    return img
+def wavedec2_img(img, wavelet, w2d_ordering):
+    if w2d_ordering=='NS':
+        ws = pywt.wavedec2(img,'haar')
+        return concat_w2d(ws)
+    elif w2d_ordering == 'S':
+        for t in range(2):
+            for y in range(np.shape(img)[1]):
+                img[y] = np.concatenate(pywt.wavedec(img[y], wavelet))
+            img = np.transpose(img)
+        return img
+    else:
+        raise Exception('w2d_ordering should be either "S" or "NS"; got : ' + w2d_ordering)
 
