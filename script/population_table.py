3228a98310ac31174e893e7ed3aabe28ca55be0b
Ubuntu <ubuntu@ip-172-31-37-237.us-west-2.compute.internal>
2015-08-12 00:17:04 +0000

    add pop table

---
+++ script/population_table.py
@@ -0,0 +1,8 @@
+class PopulationTable:
+    def __init__(self,coverage=range(30,61)):
+        
+    def add_event(self,x):
+        for i in coverage:
+            if x*10 >= i:
+                
+                above[i] = 
