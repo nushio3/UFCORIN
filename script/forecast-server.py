e404ce73b3620a8cfd2806987db9d406c7afe0e4
Ubuntu <ubuntu@ip-172-31-37-237.us-west-2.compute.internal>
2015-08-12 13:29:34 +0000

    forecast server bugfix

--- script/forecast-server.py
+++ script/forecast-server.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 
-import datetime
+import datetime,time
 import subprocess
 
 epoch = datetime.datetime(2015,1,1)
-now = datetime.datetime.now()
 
 last_diff=None
 while True:
+    now = datetime.datetime.now()
     diff = int((now-epoch).total_seconds())//(60*12)
     if last_diff < diff:
         last_diff=diff
         subprocess.call('time ./realtime_forecast.py -r do',shell=True)
+    time.sleep(10)
