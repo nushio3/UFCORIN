7fd27ecc1fcfa661259b7dc3c80461c36cc9d1dc
Ubuntu <ubuntu@ip-172-31-37-237.us-west-2.compute.internal>
2015-12-26 02:15:38 +0000

    implement adversarial norm.

--- script/suntomorrow-DCGAN/main.py
+++ script/suntomorrow-DCGAN/main.py
@@ -29,8 +29,8 @@ import numpy
 parser = argparse.ArgumentParser()
 parser.add_argument('--gpu', '-g', default=0, type=int,
                     help='GPU ID')
-parser.add_argument('--norm', default='dcgan',
-                    help='Use dcgan/L2 norm.')
+parser.add_argument('--norm', default='CA',
+                    help='Use dcgan/L2/CA norm.')
 parser.add_argument('--epoch0', default=0,type=int,
                     help='set epoch counter')
 parser.add_argument('--fresh-start', action='store_true',
@@ -173,11 +173,6 @@ class Evolver(chainer.Chain):
         #return Variable(xp.zeros((batchsize, 1, 128,128), dtype=np.float32))
 
 
-
-
-
-
-
 class Discriminator(chainer.Chain):
     def __init__(self):
         super(Discriminator, self).__init__(
@@ -186,7 +181,6 @@ class Discriminator(chainer.Chain):
             c1 = L.Convolution2D(64, 128, 4, stride=2, pad=1, wscale=0.02*math.sqrt(4*4*64)),
             c2 = L.Convolution2D(128, 256, 4, stride=2, pad=1, wscale=0.02*math.sqrt(4*4*128)),
             c3 = L.Convolution2D(256, 512, 4, stride=2, pad=1, wscale=0.02*math.sqrt(4*4*256)),
-            l4l = L.Linear(8*8*512, 2, wscale=0.02*math.sqrt(8*8*512)),
             bn0 = L.BatchNormalization(64),
             bn1 = L.BatchNormalization(128),
             bn2 = L.BatchNormalization(256),
@@ -199,11 +193,12 @@ class Discriminator(chainer.Chain):
         h = elu(x)     # no bn because images from generator will katayotteru?
         h = elu(self.bn1(self.c1(h), test=test))
         h = elu(self.bn2(self.c2(h), test=test))
-        h = elu(self.bn3(self.c3(h), test=test))
-        l = self.l4l(h)
-        return l
+        h = self.bn3(self.c3(h), test=test)
+        return h
 
-
+def d_norm(dis, img1, img2):
+    dx = dis(img1) - dis(img2)
+    return dx**2 / dx.size
 
 
 
@@ -243,22 +238,30 @@ def load_movie():
 
 global coord_image
 coord_image=None
-def create_batch(current_movie_in, current_movie_out):
+def create_batch(train_offset,current_movie_in, current_movie_out):
     global coord_image
-    for t in range(n_timeseries):
+    for t in range(train_offset, n_timeseries):
         if current_movie_in[t] is None:
             return None
         if current_movie_out[t] is None:
             return None
-    h,w = current_movie_in[0].shape
+    h,w = current_movie_in[train_offset].shape
+
+    while True:
+        rnd_t = train_offset + n_timeseries-1 + int(round(np.random.normal(16.0)))
+        if rnd_t < 0 or rnd_t >= len(current_movie_out): continue
+        if current_movie_out[rnd_t] is None: continue
+        break
 
     if coord_image is None:
         coord_image = np.zeros((2,h, w), dtype=np.float32)
 
-        for y in range(h):
-            for x in range(w):
-                coord_image[0,y,x] = (float(x)-w/2)/w
-                coord_image[1,y,x] = (float(y)-h/2)/h
+        for iy in range(h):
+            for ix in range(w):
+                x = 2*float(ix - w/2)/w
+                y = 2*float(iy - h/2)/h
+                coord_image[0,y,x] = x**2 + y**2
+                coord_image[1,y,x] = x
     
 
     pw=patch_pixelsize
@@ -266,12 +269,12 @@ def create_batch(current_movie_in, current_movie_out):
 
     ret_in  = np.zeros((batchsize, n_timeseries-1, ph, pw), dtype=np.float32)
     ret_out = np.zeros((batchsize, 1, ph, pw), dtype=np.float32)
+    ret_other = np.zeros((batchsize, 1, ph, pw), dtype=np.float32)
     
     for j in range(batchsize):
-        oh, ow = current_movie_in[0].shape
-        left  = np.random.randint(ow-pw)
-        top   = np.random.randint(oh-ph)
-        for t in range(n_timeseries):
+        left  = np.random.randint(w-pw)
+        top   = np.random.randint(h-ph)
+        for t in range(train_offset, n_timeseries):
             if t==n_timeseries -1:
                 ret_out[j,0,:,:]=current_movie_out[t][top:top+ph, left:left+pw]
             else:
@@ -280,7 +283,8 @@ def create_batch(current_movie_in, current_movie_out):
                 ret_in[j,t,:,:]=coord_image[0,top:top+ph, left:left+pw]
             if t==1:
                 ret_in[j,t,:,:]=coord_image[1,top:top+ph, left:left+pw]
-    return (ret_in, ret_out)
+        ret_other[j,0,:,:]=current_movie_out[rnd_t][top:top+ph, left:left+pw]
+    return (ret_in, ret_out, ret_other)
 
 
 # map a seris of image to image at the next, using the chainer function evol
@@ -389,13 +393,13 @@ def train_dcgan_labeled(evol, dis, epoch0=0):
             evol_scores = {}
             matsuoka_shuzo = {}
             shuzo_evoke_timestep = []
-            difficulties = ['normal','hard', 'hard']
+            difficulties = ['normal','hard']
             for difficulty in difficulties:
                 evol_scores[difficulty] = [0.0]
                 matsuoka_shuzo[difficulty] = True
             for train_offset in range(0,n_movie-n_timeseries):
                 for difficulty in difficulties:
-                    movie_clip = current_movie[train_offset:train_offset+n_timeseries]
+                    movie_clip = current_movie
                     if not matsuoka_shuzo[difficulty]:
                         # Doushitesokode yamerunda...
                         continue
@@ -406,15 +410,16 @@ def train_dcgan_labeled(evol, dis, epoch0=0):
                     if difficulty == 'normal':
                         movie_clip_in = movie_clip
                     else:
-                        movie_clip_in = prediction_movie[train_offset:train_offset+n_timeseries]
-                    maybe_dat = create_batch(movie_clip_in, movie_clip)
+                        movie_clip_in = prediction_movie
+                    maybe_dat = create_batch(train_offset,movie_clip_in, movie_clip)
                     if not maybe_dat : 
                         print "Warning: skip offset", train_offset, "because of unavailable data."
                         continue
-                    data_in, data_out = maybe_dat
+                    data_in, data_out, data_other = maybe_dat
                     movie_in =  Variable(cuda.to_gpu(data_in))
                     movie_out = Variable(cuda.to_gpu(data_out))
-                    
+                    movie_other = Variable(cuda.to_gpu(data_other))
+
                     movie_out_predict = evol(movie_in)
 
                     vis_kit[difficulty] = (movie_in.data.get(),
@@ -430,7 +435,11 @@ def train_dcgan_labeled(evol, dis, epoch0=0):
                         # train discriminator
                         yl_train = dis(movie_in,movie_out)
                         L_dis += F.softmax_cross_entropy(yl_train, Variable(xp.zeros(batchsize, dtype=np.int32)))
-
+                    elif args.norm == 'CA':
+                        yl = d_norm(dis, movie_out, movie_out_predict)
+                        L_evol = yl
+                        L_dis  = -yl
+                        L_dis  += d_norm(dis, movie_out, movie_other)
                     else:
                         L2norm = (movie_out - movie_out_predict)**2
                         yl = F.sum(L2norm) / L2norm.data.size
@@ -440,20 +449,22 @@ def train_dcgan_labeled(evol, dis, epoch0=0):
                     evol_scores[difficulty] += [L_evol.data.get()] # np.average(F.softmax(yl).data.get()[:,0])
     
                     
-                    
+                    # stop learning on normal mode.
                     o_evol.zero_grads()
                     L_evol.backward()
                     o_evol.update()
                     
-                    if args.norm == 'dcgan':
+                    if args.norm == 'dcgan' or args.norm == 'CA':
                         o_dis.zero_grads()
                         L_dis.backward()
                         o_dis.update()
                     
+                    movie_in.unchain_backward()
                     movie_out_predict.unchain_backward()
+                    movie_other.unchain_backward()
                     yl.unchain_backward()
                     L_evol.unchain_backward()
-                    if args.norm == 'dcgan':
+                    if args.norm == 'dcgan' or args.norm == 'CA':
                         yl_train.unchain_backward()
                         L_dis.unchain_backward()
     
@@ -466,7 +477,7 @@ def train_dcgan_labeled(evol, dis, epoch0=0):
                     prediction_movie[train_offset+n_timeseries-1] = evolve_image(evol,prediction_movie[train_offset: train_offset+n_timeseries-1])
 
                     # prevent too much learning from noisy prediction.
-                    if len(evol_scores['hard'])>=5 and np.average(evol_scores['hard'][-5:-1]) > 5 * np.average(evol_scores['normal']):
+                    if len(evol_scores['hard'])>=3 and np.average(evol_scores['hard'][-5:-1]) > 5 * np.average(evol_scores['normal']):
                         # Zettaini, akiramennna yo!
                         # matsuoka_shuzo['hard'] = False
                         shuzo_evoke_timestep += [train_offset]
